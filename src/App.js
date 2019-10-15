9e81d1d43723b0726f4e2786ab4ed381b1b84990
ratikshetty <ratikshetty@gmail.com>
2019-10-14 19:34:02 -0700

    Added delete Tags functionality and UI tweaks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
 import Article from './comp/article'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBlog } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="">
-      <header className="">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Blog React Application is under maintainance!!!
-          <h1>COMING SOON!!!!!!!</h1>
-          
-        </p> */}
-      
-       
-      </header>
-      <div style={{padding:50+'px', }}>
-        <div className="container-fluid">
-        <Article/>
+      <div className='row container-fluid p-0 m-0 mb-5' style={{ position: 'fixed', zIndex: '1', background: '#004d80', color: 'goldenrod' }}>
+        <div className='col-md-1 text-right pt-1 pb-1 pl-4'>
+          <h1><FontAwesomeIcon icon={faBlog} /></h1>
         </div>
-      
+        <div className='col-md-11 pt-1 pb-1 pl-4 m-auto text-left'>
+          <h2>React-Blog!!!</h2>
+        </div>
+      </div>
+      <div style={{ padding: '50px', paddingTop: '100px' }}>
+        <div className="container-fluid">
+
+          <Article />
+        </div>
+
       </div>
     </div>
   );
